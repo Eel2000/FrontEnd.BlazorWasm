8ae20572a46631f80761211f2fe82b2ff493d273
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-04-30 13:52:48 +0100

    🚧adding modals and row clicking

--- FrondEnd.Shared/Utils/BaseUrls.cs
+++ FrondEnd.Shared/Utils/BaseUrls.cs
@@ -13,8 +13,8 @@ namespace FrondEnd.Shared.Utils
     {
 #if DEBUG
         public const string BASE_URL = "https://localhost:7247";//dev api url for the project change it if your env gives you different url in the launch settings file
-#elif RELEASE
-        public const string BASE_URL =""; // prod api url if your backend is hosted somewhere on the internet put that base url here.
+#elif REALESE
+        public const string BASE_URL =" https://apitest.processcloud.net/PFAPI_Frontend"; // prod api url if your backend is hosted somewhere on the internet put that base url here.
 #endif
     }
 }
